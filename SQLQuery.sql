348e008f49876fdd8035cd926e95347ea6858d61
kennedimalheiros <kennedimalheiros@gmail.com>
2012-04-10 18:41:00 -0300

    Correção tabela caixa

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -5,9 +5,8 @@ use caixa
 -- -----------------------------------------------------
 
 
-
 CREATE  TABLE usuarios (
-  cod INT Identity(1,1) NOT NULL ,
+  cod INT Identity(1,1) ,
   nome VARCHAR(60) NOT NULL ,
   usuario VARCHAR(45) NOT NULL ,
   senha VARCHAR(45) NOT NULL ,
@@ -24,23 +23,22 @@ INSERT INTO usuarios (nome, usuario, senha, data, nivel) VALUES ('master', 'Adim
 -- -----------------------------------------------------
 
 CREATE  TABLE caixas (
-  cod INT NOT NULL ,
-  dataAbertura DATETIME NOT NULL ,
-  dataFechamento DATETIME NULL ,
-  usuarioAbertura INT NOT NULL ,
-  usuarioFechamento INT NULL ,
-  valorAbertura DECIMAL(8,2) NOT NULL ,
-  valorFechamento DECIMAL(8,2) NULL ,
+  cod INT Identity(1,1) ,
+  data DATETIME NULL ,
+  usuario INT NOT NULL ,
+  valor DECIMAL(8,2) NOT NULL ,
   periodo BIT NOT NULL, -- COMMENT 0 Dia  1 Noite\n
+  situacao BIT NOT NULL, -- 0 Aberto  1 Fechado
+  codcaixa INT NOT NULL, -- Cod referente a caixa
   PRIMARY KEY (cod),
-  FOREIGN KEY (usuarioAbertura)
+  FOREIGN KEY (usuario)
 	REFERENCES usuarios (cod), 
-  FOREIGN KEY (usuarioFechamento)
-	REFERENCES usuarios (cod) 	
+  FOREIGN KEY (codcaixa)
+	REFERENCES caixas (cod) 	
   )
 
 INSERT INTO caixas (dataAbertura, dataFechamento, usuarioAbertura, usuarioFechamento, valorAbertura, valorFechamento, periodo)
-					values (GETDATE(),null, 1,null, 100, null, 0)
+					values (GETDATE(),null, 1,null, 100, null, 0, 0)
 
 -- -----------------------------------------------------
 -- Table Tipos
@@ -49,24 +47,29 @@ INSERT INTO caixas (dataAbertura, dataFechamento, usuarioAbertura, usuarioFecham
 --COMMENT = 'Tipo Operação\nSangria\nSuprimento\nCompra material\nCompra etc.'
 
 CREATE  TABLE tipos (
-  cod INT NOT NULL ,
+  cod INT Identity(1,1) ,
   descricao VARCHAR(45) NOT NULL ,
   acao BIT NOT NULL, -- COMMENT 0 Debito 1 Credito
   PRIMARY KEY (cod) 
   )
+  INSERT INTO tipos (descricao, acao) VALUES ('ABRI_CAIXA', 1)
+  INSERT INTO tipos (descricao, acao) VALUES ('SANGRIA', 0)
+  INSERT INTO tipos (descricao, acao) VALUES ('COMPRA', 0)
+  INSERT INTO tipos (descricao, acao) VALUES ('SUPRIMENTO', 1)
 
-
-
+ 
+  
 -- -----------------------------------------------------
 -- Table Movimentacões
 -- -----------------------------------------------------
 
 CREATE  TABLE movimentacoes (
-  cod INT NOT NULL ,
+  cod INT Identity(1,1) ,
   data DATETIME NOT NULL ,
   usuario INT NOT NULL ,
-  tipo INT NOT NULL, -- COMMENT Suprimento, Sangria
+  tipo INT NOT NULL, -- COMMENT Suprimento, Sangria...
   caixa INT NOT NULL, -- COMMENT 0 Dia  1 Noite
+  valor DECIMAL(8,2) NOT NULL,
   PRIMARY KEY (cod),
   FOREIGN KEY (usuario)
 	REFERENCES usuarios (cod),
@@ -74,6 +77,19 @@ CREATE  TABLE movimentacoes (
 	REFERENCES caixas (cod)   
   )
 
+INSERT INTO movimentacoes (data, usuario, tipo, caixa, valor) 
+				   VALUES (GETDATE(),  1,   1,      1,    100 ) 
 
-Create procedure SpAberturaCaixa
+INSERT INTO movimentacoes (data, usuario, tipo, caixa, valor) 
+				   VALUES (GETDATE(),  1,   1,      1,    100 ) 
+
+
+
+Create procedure SpMovimentacao
 AS
+Begin Tran
+if 
+
+
+
+select * from caixas where dataFechamento != NULL
