86850449e9100eec6680a32c4b5d6b500bd4d327
kennedimalheiros <kennedimalheiros@gmail.com>
2012-04-09 21:10:44 -0300

    Concluindo conexão com o Banco de dados Sql Server.

--- LivroCaixa/src/br/com/caixa/conexao/SqlServer.java
+++ LivroCaixa/src/br/com/caixa/conexao/SqlServer.java
@@ -4,10 +4,25 @@
  */
 package br.com.caixa.conexao;
 
+import java.sql.*; // Importando biblioteca java SQL para conexões.
+
 /**
+ * Criando Conexão com o Banco de dados SQL Server
  *
- * @author www
+ * @author Kennedi Paulo S. Malheiros
+ * @author Colaboradores
+ * @version 1.0 09/04/2012
+ * @since 1.0
  */
 public class SqlServer {
-    
+
+    public static Connection getConexaoSQL() throws SQLException {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            return DriverManager.getConnection("jdbc:sqlserver://localhost:1433; databaseName=caixa; user=kennedysql; password=123");
+
+        } catch (ClassNotFoundException e) {
+            throw new SQLException(e.getMessage());
+        }
+    }
 }
